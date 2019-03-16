e5a2396034ae3bb2546ea79403bb8ff0efae26a7
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-16 05:04:34 -0400

    removed some code that was causing a bug with re-auth not seeing the value from the props for the authPassword because it didnt think it needed to update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Backdrop from './Backdrop';
 import * as Styles from './_modal-styles';
 
-class modal extends Component {
+const modal = (props) => {
 
-    // Only class component for performance reasons
-
-    // preventing the order summary from updating since it is a child
-    // unless of course we are showing the modal, then it should update
-    shouldComponentUpdate(nextProps) {
-        return nextProps.show !== this.props.show;
-    }
-
-    render(){
         let isHidden;
-        this.props.show ? isHidden = false : isHidden = true;
+        props.show ? isHidden = false : isHidden = true;
 
         return(
             <>
-            <Backdrop show={this.props.show} deactive={this.props.deactive}/>
+            <Backdrop show={props.show} deactive={props.deactive}/>
             <Styles.Modal hide={isHidden}>
-                {this.props.children}
+                {props.children}
             </Styles.Modal>
             </>
         );
-    }
 }
 
 export default modal;
